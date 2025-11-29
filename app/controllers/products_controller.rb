a59aa3ee6202a0146d3c5c129f60ac146c104bbe
Fernanda Fonseca <fernanda.fon.nieto@gmail.com>
2025-11-29 18:38:47 +0000

    [feat]:lista_produtos

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -2,5 +2,18 @@ class ProductsController < ApplicationController
     def show
         @product = Product.find(params[:id])
     end
+
+    def new
+        @product = Product.new
+    end
+
+    def create
+        @product = Product.new(name: params[:name], price: params[:price], quantity: params[:quantity])
+        if @product.save
+            redirect_to "/products/#{@product.id}"
+        else
+            render :new, status: :unprocessable_entity
+        end
+    end
 end
 
